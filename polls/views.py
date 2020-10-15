b8ecb60a0be4be8d5bc592c8435c8d1d1c3e4176
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2020-10-15 17:07:18 -0300

    Update view index

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -22,3 +23,11 @@ def index(request):
     return HttpResponse(output)
 
 # Leave the rest of the views (detail, results, vote) unchanged
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
