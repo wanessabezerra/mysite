264c5f17c5e82245736ccecc76cecbd909bc2eac
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2020-10-15 17:11:49 -0300

    Update view index

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+
 
 from .models import Question
 
@@ -31,3 +34,15 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+    def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
